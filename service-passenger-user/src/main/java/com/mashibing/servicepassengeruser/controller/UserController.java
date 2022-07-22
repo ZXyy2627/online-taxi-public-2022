4f3fc5ef7e5cd1bbb1b44bd69588a3af21d924b1
ZXyy2627 <1375423395@qq.com>
2022-07-22 14:18:14 +0800

    将internal-common提取到父pom中

---
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.mashibing.servicepassengeruser.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName UserController
+ * @Description 请描述类的业务用途
+ * @Author zhangxiang
+ * @Date 2022/7/22
+ * @Version 1.0
+ **/
+@RestController
+public class UserController {
+
+
+}
