464e63a1d39d5b23b54aab29f287131e43c6d8b9
ZXyy2627 <1375423395@qq.com>
2022-07-22 14:07:45 +0800

    新建service-passenger-user项目

---
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/ServicePassengerUserApplication.java
@@ -0,0 +1,21 @@
+package com.mashibing.servicepassengeruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName ServicePassengerUserApplication
+ * @Description 请描述类的业务用途
+ * @Author zhangxiang
+ * @Date 2022/7/22
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServicePassengerUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePassengerUserApplication.class, args);
+    }
+}
---
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.mashibing.servicepassengeruser.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName TestController
+ * @Description 请描述类的业务用途
+ * @Author zhangxiang
+ * @Date 2022/7/22
+ * @Version 1.0
+ **/
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "service-passenger-user";
+    }
+}
